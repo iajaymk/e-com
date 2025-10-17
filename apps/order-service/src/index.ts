b984537cc5ca7aceff814c840c1012dbad425a4c
Ajay Kumar <ajaykumarmkaju@gmail.com>
2025-10-17 19:53:46 +0530

    Add payment service

--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,6 +1,15 @@
 import Fastify from "fastify";
+import { request } from "http";
 
 const fastify = Fastify();
+
+fastify.get("/health", (request, reply) => {
+  return reply.status(200).send({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
 
 const start = async () => {
   try {
